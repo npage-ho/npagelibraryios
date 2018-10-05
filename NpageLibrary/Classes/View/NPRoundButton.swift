371875ade36f4395d0474a3a3d536c46d8054485
ho <ho@npage.co.kr>
2018-10-05 16:55:27 +0900

    update RoundButton bug fix

--- NpageLibrary/Classes/View/NPRoundButton.swift
+++ NpageLibrary/Classes/View/NPRoundButton.swift
@@ -15,15 +15,22 @@ open class NPRoundButton: NPLocalizationButton {
             self.layer.cornerRadius = self.cornerRadius
         }
     }
+    var defaultBgColor: UIColor?
     @IBInspectable public dynamic var highlightBgColor: UIColor?
     @IBInspectable public dynamic var selectedBgColor: UIColor?
     @IBInspectable public dynamic var lineWidth: Int = 0
     @IBInspectable public dynamic var fillColor: UIColor = UIColor.clear
     
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        defaultBgColor = self.backgroundColor
+    }
+    
     override open var isHighlighted: Bool {
         didSet {
             if self.highlightBgColor != nil {
-                backgroundColor = isHighlighted ? self.highlightBgColor : self.backgroundColor
+                backgroundColor = isHighlighted ? self.highlightBgColor : defaultBgColor
             }
         }
     }
@@ -31,7 +38,7 @@ open class NPRoundButton: NPLocalizationButton {
     override open var isSelected: Bool {
         didSet {
             if self.selectedBgColor != nil {
-                backgroundColor = isSelected ? self.selectedBgColor : self.backgroundColor
+                backgroundColor = isSelected ? self.selectedBgColor : defaultBgColor
             }
         }
     }
